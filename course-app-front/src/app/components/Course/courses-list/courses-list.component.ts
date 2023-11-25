5cbd528bf405a989436392bd96b2ce23148ea4f3
NicolasHerbigniaux <la198386@student.helha.be>
2023-11-25 14:09:46 +0100

    Fonction getCourses pour le service de courses et affichage

--- course-app-front/src/app/components/Course/courses-list/courses-list.component.ts
+++ course-app-front/src/app/components/Course/courses-list/courses-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IPayPalConfig, NgxPaypalComponent } from 'ngx-paypal';
+import { CourseService } from 'src/app/services/course.service';
 import { PaypalService } from 'src/app/services/paypal.service';
+import { Course } from 'src/models/course';
 
 @Component({
   selector: 'app-courses-list',
@@ -15,9 +17,18 @@ export class CoursesListComponent implements OnInit {
   showSuccess?: boolean;
   paypalInit:boolean = false;
 
-  constructor(private payPalService: PaypalService) {}
+  courses: Course[] | undefined = [];
+
+  constructor(private payPalService: PaypalService, private courseService: CourseService) {}
 
   ngOnInit(): void {
     this.payPalConfig = this.payPalService.initConfig('10');
+    this.getCourses();
+  }
+
+  getCourses(): void {
+    this.courseService.getCourses().subscribe((courses: Course[] | undefined) => {
+      this.courses = courses;
+    });
   }
 }
--- course-app-front/src/app/services/course.service.ts
+++ course-app-front/src/app/services/course.service.ts
@@ -1,15 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { Course } from 'src/models/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
+  private courseApiUrl = 'http://localhost:8080/api/course';
 
   constructor(private http: HttpClient) { }
 
   addCourse(course:Course) {
-    return this.http.post('http://localhost:8080/api/course', course, { responseType: 'text' });
+    return this.http.post(this.courseApiUrl, course, { responseType: 'text' });
+  }
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.courseApiUrl}`).pipe(
+      tap((courses: Course[]) => this.log(courses)),
+      catchError((error: Error) => this.handleError(error, undefined))
+    );
+  }
+
+  private log(response: Course | Course[] | boolean | undefined): void {
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
   }
 }
