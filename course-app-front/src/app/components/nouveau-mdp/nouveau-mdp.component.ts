af2a6133529ec5e98e2c76f57229522bab65140a
AmauryBnd <la198582@student.helha.be>
2023-12-01 17:40:11 +0100

    création d'un formulaire pour creer son propre mdp

---
+++ course-app-front/src/app/components/nouveau-mdp/nouveau-mdp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-nouveau-mdp',
+  templateUrl: './nouveau-mdp.component.html',
+  styleUrls: ['./nouveau-mdp.component.css'],
+  providers: [MessageService]
+})
+export class NouveauMdpComponent {
+
+}
