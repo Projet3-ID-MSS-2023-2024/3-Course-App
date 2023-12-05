19b686264120b1b1bdef29fe164ddb2c13de75a5
NicolasHerbigniaux <la198386@student.helha.be>
2023-12-05 14:59:58 +0100

    Création du rest controller de Resultat
    et fonction d'ajout

---
+++ course-app/src/main/java/com/example/courseapp/restControllers/ResultatRestController.java
@@ -0,0 +1,30 @@
+package com.example.courseapp.restControllers;
+
+import com.example.courseapp.dto.CourseRequest;
+import com.example.courseapp.models.Adresse;
+import com.example.courseapp.models.Course;
+import com.example.courseapp.models.Resultat;
+import com.example.courseapp.models.Utilisateur;
+import com.example.courseapp.services.CourseService;
+import com.example.courseapp.services.ResultatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/course")
+public class ResultatRestController {
+
+    @Autowired
+    CourseService courseService;
+
+    @Autowired
+    ResultatService resultatService;
+
+    @PostMapping
+    public Resultat add(@RequestBody Resultat newResultat) {
+        return resultatService.add(newResultat);
+    }
+}
