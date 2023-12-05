39ececd53311c6e659df5da33038157c6dab49a2
NicolasHerbigniaux <la198386@student.helha.be>
2023-12-05 17:03:40 +0100

    Fonction getResultatsByUserId dans le frontend

--- course-app-front/src/app/services/resultat.service.ts
+++ course-app-front/src/app/services/resultat.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, of, tap } from 'rxjs';
 import { Resultat } from 'src/models/resultat';
 
 @Injectable({
@@ -12,5 +13,21 @@ export class ResultatService {
 
   add(resultat: Resultat) {
     return this.http.post(this.resultatApiUrl, resultat, { responseType: 'text' });
+  }
+
+  getResultatsByUserId(id: number): Observable<Resultat[]>{
+    return this.http.get<Resultat[]>(`${this.resultatApiUrl}/${id}`).pipe(
+      tap((Resultats: Resultat[]) => this.log(Resultats)),
+      catchError((error: Error) => this.handleError(error, undefined))
+    );
+  }
+
+  private log(response: Resultat | Resultat[] | boolean | undefined): void {
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
   }
 }
