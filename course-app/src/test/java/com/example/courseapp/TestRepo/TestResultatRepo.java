3c14a7b9e4a34d5ed9cd8cc6fa40a0e5500967ec
NicolasHerbigniaux <la198386@student.helha.be>
2024-01-22 15:53:27 +0100

    Test de la fonction findAllByUtilisateur du repo résultat

--- course-app/src/test/java/com/example/courseapp/TestRepo/TestResultatRepo.java
+++ course-app/src/test/java/com/example/courseapp/TestRepo/TestResultatRepo.java
@@ -1,7 +1,9 @@
 package com.example.courseapp.TestRepo;
 
 import com.example.courseapp.models.Resultat;
+import com.example.courseapp.models.Utilisateur;
 import com.example.courseapp.repo.ResultatRepo;
+import com.example.courseapp.repo.UtilisateurRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -16,6 +19,9 @@ public class TestResultatRepo {
 
     @Autowired
     ResultatRepo repoResultat;
+
+    @Autowired
+    UtilisateurRepo repoUtilisateur;
 
     /*** Test pour récupérer tous les résultats et voir si la liste n'est pas vide ***/
     @Test
@@ -37,10 +43,45 @@ public class TestResultatRepo {
         Optional<Resultat> resultat = repoResultat.findById(1);
         assertTrue(!resultat.isEmpty());
     }
+
     /*** Test pour récupérer un résultat par l'id et voir si le résultat n'est pas null ***/
     @Test
     public void getResultatNotNull(){
         Optional<Resultat> resultat = repoResultat.findById(1);
         assertTrue(resultat != null);
+    }
+
+    /*** Test pour récupérer les résultat par l'id du user et vérifie si les résultats ne sont pas vides ***/
+    @Test
+    public void getResultatByUser(){
+        Optional<Utilisateur> user = repoUtilisateur.findById(1);
+        List<Resultat> resultats = repoResultat.findAllByUtilisateur(user);
+        assertTrue(!resultats.isEmpty());
+    }
+
+    /*** Test pour récupérer les résultat par l'id du user et vérifie si le résultats ne sont pas null ***/
+    @Test
+    public void getResultatByUserNotNull(){
+        Optional<Utilisateur> user = repoUtilisateur.findById(1);
+        List<Resultat> resultats = repoResultat.findAllByUtilisateur(user);
+        assertTrue(resultats != null);
+    }
+
+    /*** Test pour récupérer les résultats par l'id du user et vérifie si le nombre de résultats correspond aux nombre de courses payées par ce user ***/
+    @Test
+    public void getResultatByUserSize(){
+        Optional<Utilisateur> user = repoUtilisateur.findById(1);
+        List<Resultat> resultats = repoResultat.findAllByUtilisateur(user);
+        assertTrue(resultats.size() == 3);
+    }
+
+    /*** Test pour récupérer un résultat par l'id du user et vérifie si la course correspondant est bien celle payée ***/
+    @Test
+    public void getResultatByUserCourse() {
+        Optional<Utilisateur> user = repoUtilisateur.findById(1);
+        List<Resultat> resultats = repoResultat.findAllByUtilisateur(user);
+        assertEquals(2, resultats.get(0).getCourse().getId());
+        assertEquals(1, resultats.get(1).getCourse().getId());
+        assertEquals(3, resultats.get(2).getCourse().getId());
     }
 }
